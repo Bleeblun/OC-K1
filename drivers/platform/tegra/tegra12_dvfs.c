e72019bd282d2540be60b13c75a443c8463c5dfd
Stu <bleeblun_ste@hotmail.com>
2021-03-27 17:58:12 +0000

    Add Overclock Tweaks

--- drivers/platform/tegra/tegra12_dvfs.c
+++ drivers/platform/tegra/tegra12_dvfs.c
@@ -164,8 +164,12 @@ void __init tegra12x_vdd_cpu_align(int step_uv, int offset_uv)
 
 /* CPU DVFS tables */
 static unsigned long cpu_max_freq[] = {
-/* speedo_id	0	 1	  2	   3	    4	     5	      6		7	 8*/
+/* speedo_id   0   1        2        3        4        5        6        7        8*/
+#if defined(CONFIG_TEGRA_CPU_OVERCLOCK)
+		2014500, 2320500, 2116500, 2524500, 1811000, 2499000, 1912500, 1912500, 2116500,
+#else
 		2014500, 2320500, 2116500, 2524500, 1811000, 2218500, 1912500, 1912500, 2116500,
+#endif
 };
 
 static struct cpu_cvb_dvfs cpu_cvb_dvfs_table[] = {
@@ -780,8 +784,12 @@ static int resolve_core_override(int min_override_mv)
 
 /* GPU DVFS tables */
 static unsigned long gpu_max_freq[] = {
-/* speedo_id	0	1	2	 3	4	5	6*/
+/* speedo_id 0	1		 2	 	  3		  4		  5		  6*/
+#if defined(CONFIG_TEGRA_GPU_OVERCLOCK)
+		648000, 1008000, 1008000, 780000, 804000, 756000, 1008000
+#else
 		648000, 852000, 1008000, 780000, 804000, 756000, 852000
+#endif
 };
 static struct gpu_cvb_dvfs gpu_cvb_dvfs_table[] = {
 	{
